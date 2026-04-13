1af9163fd0f861806d8513391d752ae45296e423
atul-100 <ak6899222@gmail.com>
2026-04-13 08:15:44 +0530

    vinalv122

--- client/app.config.js
+++ client/app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     expo: {
       name: "Klassin",
       slug: "klassin",
-      version: "1.2.1",
+      version: "1.2.2",
       orientation: "portrait",
       icon: "./assets/images/icon2.png",
       scheme: "klassin",
